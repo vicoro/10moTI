675b853ac7ca95e65285d5c4c0eeb8ba474a6331
oscar Arellano <oscar_alejandro099@outlook.es>
2023-09-22 15:13:58 -0600

    Se ingreso el nav y el menú lateral

---
+++ alumnos/Oscar/POS/vistas/modulos/cabezote.php
@@ -0,0 +1,83 @@
+<header class="main-header">
+
+    <!--=============================================
+        LOGOTIPO
+    ================================================-->
+
+    <a href="" class="logo">
+
+        <!-- logo mini -->
+            <span class="logo-mini">
+
+               <!-- <img src="vistas/img/plantilla/icono-blanco.png" class="
+                img-responsive" alt="no sirvio" style="width:40px">-->
+                <b style="font-size:40px">C</b>
+
+            </span>
+
+        <!-- logo normal -->
+            <span class="logo-lg">
+            
+                <!--<img src="vistas/img/plantilla/icono-blanco-lineal.png" class="
+                img-responsive" alt="no sirvio" style="padding:0px 50px">-->
+                <b style="font-size:40px">C</b><b style="font-size:40px">IMA</b>
+            </span>
+
+    </a>
+
+    <!--=============================================
+        NAVEGACIÓN
+    ================================================-->
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <!--Botón de navegación-->
+
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+
+            <span class="sr-only">Toggle navigation</span>
+
+        </a>
+
+        <!--Perfil de usuario-->
+
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+                
+                <li class="dropdown user user-menu">
+
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                        <!--<img src="vistas/img/usuarios/default/anonymous.png" class="user-img" style="width:25px; height:20px;">-->
+                        <i class="fa-solid fa-face-smile" style="font-size:15px"></i>
+
+                        <span class="hidden-xs">Usuario Administrador</span>
+                    </a>
+
+                    
+                    <!--Perfil de usuario-->
+
+                    <ul class="dropdown-menu">
+
+                        <li class="user-body">
+
+                            <div class="pull-right">
+
+                                <a href="" class="btn btn-default btn-flat">Salir</a>
+
+                            </div>
+
+                        </li>
+
+                    </ul>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+    </nav>
+
+
+<header>
---
+++ alumnos/Oscar/POS/vistas/modulos/menu.php
@@ -0,0 +1,73 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <ul class="sidebar-menu">
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa-solid fa-house"></i>
+                    <span>Inicio</span>
+                </a>
+
+            </li>
+            <li>
+
+                <a href="">
+
+                    <i class="fa-solid fa-user" style="font-size:15px"></i>
+                    <span>Usuarios</span>
+
+                </a>
+
+            </li>
+            <li>
+
+                <a href="">
+
+                    <i class="fa-solid fa-list" style="font-size:15px"></i>
+                    <span>Categorías</span>
+                    
+                </a>
+
+            </li>
+            <li>
+
+                <a href="">
+
+                    <i class="fa-brands fa-product-hunt"style="font-size:15px"></i>
+                    <span>Productos</span>
+                    
+                </a>
+
+            </li>
+            <li>
+
+                <a href="">
+
+                    <i class="fa-solid fa-user" style="font-size:15px"></i>
+                    <span>Clientes</span>
+                    
+                </a>
+
+            </li>
+            <li class="treeview">
+
+                <a href="">
+
+                    <i class="fa-solid fa-ellipsis-vertical" style="font-size:15px"></i>
+                    <span>Ventas</span>
+                    <span class="pull-right-container">
+                        <i class="fa-solid fa-angle-left pull-right"></i>
+                    </span>
+                    
+                </a>
+
+            </li>
+        </ul>
+
+    </section>
+
+</aside>
