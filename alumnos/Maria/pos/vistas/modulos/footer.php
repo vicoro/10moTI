ced918d512d9f53e22c89df606560b3eac8309b3
Maria Velazquez <marivel.del.97@gmail.com>
2023-09-24 19:18:57 -0600

    Avances de la semana 2 y 3

---
+++ alumnos/Maria/pos/vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+
+<footer class="main-footer">
+	<strong>Copyringht &copy;2023 <a href="https://
+		www.tutorialesatualcanse.com " target="_blank"> Tutoriales a tu alcance</a>
+	</strong>
+	 Todos los derechos reservados.
+	
+</footer>
