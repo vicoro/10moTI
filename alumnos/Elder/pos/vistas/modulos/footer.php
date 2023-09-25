9ef1071f5721f51f1adf3f1a02853fc21e1f1e81
Elder Pineda <pinedaelder68@gmail.com>
2023-09-24 20:24:16 -0600

    avances de la semana 2 y 3

---
+++ alumnos/Elder/pos/vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+
+    <strong>Copyright &copy; 2023 <a href="https://www.mundopapel.com" target="_blank">Mundo Papel</a></strong>
+
+    Todos los derechos reservados
+
+
+</footer>
